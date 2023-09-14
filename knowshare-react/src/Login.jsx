1f8e118cbda966613079322f4b6620c62996ef25
Papi <gbemialihassan@gmail.com>
2023-09-14 15:22:53 +0200

    more features

--- knowshare-react/src/Login.jsx
+++ knowshare-react/src/Login.jsx
@@ -1,49 +1,72 @@
-// Login.js
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function Login() {
-  const [formData, setFormData] = useState({ email: '', password: '' });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add login logic here (e.g., validate credentials and authenticate user)
-    console.log('Login form submitted:', formData);
-  };
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="bg-white p-8 rounded-lg shadow-md">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <input
-              type="email"
-              className="w-full px-3 py-2 rounded border"
-              placeholder="Email"
-              value={formData.email}
-              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="password"
-              className="w-full px-3 py-2 rounded border"
-              placeholder="Password"
-              value={formData.password}
-              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-              required
-            />
-          </div>
-          <div className="text-center">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-full"
-            >
-              Login
-            </button>
-          </div>
-        </form>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            // Add login logic here (e.g., validate credentials and authenticate user)
+            console.log('Login form submitted:', values);
+
+            // Call actions.setSubmitting(false) to indicate that form submission is complete
+            actions.setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="space-y-4">
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Email address
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  className="w-full px-4 py-2 rounded-lg border focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 text-gray-900 sm:text-sm"
+                  placeholder="Email"
+                />
+                <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className="w-full px-4 py-2 rounded-lg border focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 text-gray-900 sm:text-sm"
+                  placeholder="Password"
+                />
+                <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+              </div>
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className={`w-full py-2 px-4 rounded-full bg-indigo-600 text-white font-semibold text-sm ${
+                    isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-700'
+                  }`}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? 'Logging in...' : 'Login'}
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
--- knowshare-react/src/Signup.jsx
+++ knowshare-react/src/Signup.jsx
@@ -13,61 +13,61 @@ function Signup() {
   });
 
   return (
-    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
-      <h2 className="text-center text-2xl font-bold text-gray-900 mb-6">Sign in to your account</h2>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={(values, actions) => {
-          // Add signup logic here (e.g., validate input and create a new user account)
-          console.log('Signup form submitted:', values);
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            // Add signup logic here (e.g., validate input and create a new user account)
+            console.log('Signup form submitted:', values);
 
-          // Call actions.setSubmitting(false) to indicate that form submission is complete
-          actions.setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
-              <Field
-                type="email"
-                name="email"
-                autoComplete="email"
-                required
-                className="block w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 sm:text-sm"
-                placeholder="Email"
-              />
-              <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
-            </div>
-
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-              <Field
-                type="password"
-                name="password"
-                autoComplete="current-password"
-                required
-                className="block w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 sm:text-sm"
-                placeholder="Password"
-              />
-              <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className={`w-full py-2 px-4 rounded-lg bg-indigo-600 text-white font-semibold text-sm ${
-                  isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-700'
-                }`}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? 'Signing in...' : 'Sign in'}
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+            // Call actions.setSubmitting(false) to indicate that form submission is complete
+            actions.setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="space-y-4">
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Email address
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  className="w-full px-4 py-2 rounded-lg border focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 text-gray-900 sm:text-sm"
+                  placeholder="Email"
+                />
+                <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className="w-full px-4 py-2 rounded-lg border focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 text-gray-900 sm:text-sm"
+                  placeholder="Password"
+                />
+                <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+              </div>
+              <div className="text-center">
+                <button
+                  type="submit"
+                  className={`w-full py-2 px-4 rounded-full bg-indigo-600 text-white font-semibold text-sm ${
+                    isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-700'
+                  }`}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? 'Signing up...' : 'Sign Up'}
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }

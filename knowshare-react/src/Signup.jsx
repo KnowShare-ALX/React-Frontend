1b205f754b86cf8d8552e3611e75bffc8637fb3c
Papi <gbemialihassan@gmail.com>
2023-09-14 15:12:08 +0200

    sigup component validation

--- knowshare-react/src/Signup.jsx
+++ knowshare-react/src/Signup.jsx
@@ -1,66 +1,73 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function Signup() {
-  const [formData, setFormData] = useState({ email: '', password: '' });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add signup logic here (e.g., validate input and create a new user account)
-    console.log('Signup form submitted:', formData);
-  };
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required'),
+  });
 
   return (
-    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-      <form className="space-y-6" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-          <div className="mt-2">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Email"
-              value={formData.email}
-              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            />
-          </div>
-        </div>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+      <h2 className="text-center text-2xl font-bold text-gray-900 mb-6">Sign in to your account</h2>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={(values, actions) => {
+          // Add signup logic here (e.g., validate input and create a new user account)
+          console.log('Signup form submitted:', values);
 
-        <div>
-          <div className="flex items-center justify-between">
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-            <div className="text-sm">
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+          // Call actions.setSubmitting(false) to indicate that form submission is complete
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+              <Field
+                type="email"
+                name="email"
+                autoComplete="email"
+                required
+                className="block w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 sm:text-sm"
+                placeholder="Email"
+              />
+              <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
             </div>
-          </div>
-          <div className="mt-2">
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Password"
-              value={formData.password}
-              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-            />
-          </div>
-        </div>
 
-        <div>
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              <Field
+                type="password"
+                name="password"
+                autoComplete="current-password"
+                required
+                className="block w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 sm:text-sm"
+                placeholder="Password"
+              />
+              <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className={`w-full py-2 px-4 rounded-lg bg-indigo-600 text-white font-semibold text-sm ${
+                  isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-700'
+                }`}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? 'Signing in...' : 'Sign in'}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
